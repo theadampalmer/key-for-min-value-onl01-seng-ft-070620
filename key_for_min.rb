2ac4293ed08dfe0ffb683223d0fd4947e0dffd86
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 22:39:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,67 +1,13 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+def key_for_min_value(name_hash)
 
-describe "smallest hash value" do
-
-  it "does not call the `#keys` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:keys)
-
-    key_for_min_value(hash)
+end 
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
   end
-
-  it "does not call the `#values` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:values)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#min` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:min)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#sort` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:sort)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#sort_by` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:sort_by)
-
-    key_for_min_value(hash)
-  end
-
-  it "does not call the `#min_by` method" do
-    hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-    expect(hash).to_not receive(:min_by)
-
-    key_for_min_value(hash)
-  end
-
-  it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
-  end
-
-  it "returns the key of the smallest hash value example 2" do
-    expect(key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})).to eq(:blake)
-  end
-
-  it "returns nil for an empty hash" do
-    expect(key_for_min_value({})).to eq(nil)
-  end
-
+  lowest_key
 end
-
