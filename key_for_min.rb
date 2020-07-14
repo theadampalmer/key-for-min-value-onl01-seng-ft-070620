3987de9354b505475e0a2c6969f0c94dad284007
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 22:43:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,20 @@
-def key_for_min_value(name_hash)
-  lowest_key = nil
-  lowest_value = nil
-  name_hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
+def key_for_min_value(name_hash)  
+  
+  if name_hash.length == 0
+    return nil
+  else
+    array = []
+    name_hash.each do |key, value|
+      array << value
+    end
+    min_num = array[0]
+    array.each do |value|
+      min_num = value if value < min_num
+    end
+    name_hash.each do |key, value|
+      if min_num == value
+        return key
+      end
     end
   end
-  lowest_key
 end
-
-key_for_min_value(name_hash)
